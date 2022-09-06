dfb64a5a0dbadf9b1a0387bbc5a2a00b71df7018
KalelBaiao <kalelbaiao2@gmail.com>
2022-09-05 22:39:11 -0400

    atualização no modo dark

--- js/dark.js
+++ js/dark.js
@@ -1,4 +1,23 @@
 const chk = document.getElementById('chk')
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark')
+var darkMode = localStorage.getItem('darkMode?')
+
+if(darkMode == "true"){
+    chk.checked = true
+    document.body.classList.add('dark')
+} else{
+    chk.checked = false
+}
+
+chk.addEventListener('click', () => {
+    let checkboxTrueOrFalse = chk.checked
+    if (checkboxTrueOrFalse == true){
+         document.body.classList.add('dark')
+        //  console.log(checkboxTrueOrFalse)
+         localStorage.setItem('darkMode?', checkboxTrueOrFalse)
+    }else{
+        document.body.classList.remove('dark')
+        localStorage.setItem('darkMode?', checkboxTrueOrFalse)
+        // console.log(checkboxTrueOrFalse)
+    } 
+
 })
