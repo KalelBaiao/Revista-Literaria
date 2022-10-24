3b1f2050be498ac07397b519e0533f0e18e3ff7f
KalelBaiao <kalelbaiao2@gmail.com>
2022-10-23 20:53:58 -0400

    submissão atualizada

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // import { initializeApp } from 'firebase/app'
 // import { getStorage, ref, uploadBytesResumable } from 'firebase/storage'
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getStorage, ref, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js"
+import { getStorage, ref, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js"
 
 // const form = document.querySelector(".form-contato")
 
@@ -15,28 +15,66 @@ const firebaseApp = {
     measurementId: "G-FGCTN6KKW0"
 }
 
+
+const name = document.getElementById("input-name")
+const email = document.getElementById("input-email")
+const arquivo = document.getElementById("input-arquivo")
+const btnSubimit = document.getElementById("envio")
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseApp)
 const form = document.querySelector(".form-contato")
 const storage = getStorage(app)
 // console.log(storage);
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault()
     const file = document.querySelector(".file").files[0]
 
-    const storageRef = ref(storage, `Submissões/${file.name}/${file}`)
+    const storageRef = await ref(storage, `Submissões/${file.name}/${file}`)
     uploadBytesResumable(storageRef, file)
         .then(() => {
-            const name = document.getElementById("input-name")
             name.value = ''
-            const email = document.getElementById("input-email")
             email.value = ''
-            const arquivo = document.getElementById("input-arquivo")
             arquivo.value = ''
-            const btnSubimit = document.getElementById("envio")
             btnSubimit.innerText = "ENVIADO COM SUCESSO!!!"
         }).catch((error) => {
             console.log(error)
         })
+})
+
+
+
+import { getFirestore, collection, addDoc, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCEFGTE5Cy7dHWpoEV-L-vbaar-kMbD_c",
+    authDomain: "revista-literatus.firebaseapp.com",
+    projectId: "revista-literatus",
+    storageBucket: "revista-literatus.appspot.com",
+    messagingSenderId: "637824781668",
+    appId: "1:637824781668:web:7f4c8126a8a22436303e71",
+    measurementId: "G-FGCTN6KKW0"
+}
+
+const fireapp = initializeApp(firebaseConfig)
+const firestore = getFirestore(fireapp)
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const file = document.querySelector(".file").files[0]
+
+    try {
+        // setDoc(doc(firestore, "users", "new-user"), data)
+        const docRef = await addDoc(collection(firestore, "users"), {
+            nome: `${name.value}`,
+            email: `${email.value}`,
+            arquivo: `${file.name}`
+        })
+        console.log("Document written with ID: ", docRef.id)
+    } catch (e) {
+        console.error("Error adding document: ", e)
+    }
 })
